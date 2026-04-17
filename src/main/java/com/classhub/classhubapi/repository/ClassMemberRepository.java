eee52540231270a7039f0b18969f5233bd1029e0
agio7 <phonga3k54@gmail.com>
2026-04-18 06:09:26 +0700

    Cứ thế thôi

--- src/main/java/com/classhub/classhubapi/repository/ClassMemberRepository.java
+++ src/main/java/com/classhub/classhubapi/repository/ClassMemberRepository.java
@@ -2,9 +2,10 @@ package com.classhub.classhubapi.repository;
 
 import com.classhub.classhubapi.entity.ClassMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface ClassMemberRepository extends JpaRepository<ClassMember, Long> {
 
     boolean existsByUserIdAndClassroomId(Long userId, Long classroomId);
--- src/main/java/com/classhub/classhubapi/repository/ClassroomRepository.java
+++ src/main/java/com/classhub/classhubapi/repository/ClassroomRepository.java
@@ -2,9 +2,10 @@ package com.classhub.classhubapi.repository;
 
 import com.classhub.classhubapi.entity.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
 
     Optional<Classroom> findByInviteCode(String inviteCode);
